eff7842298d821e95b235c682ee1872c6b00a0c7
Livia Devolder <livia.pinheiro-extern@renault.com>
2023-06-22 00:31:26 -0300

    chore: connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { json } from 'body-parser'
 import express from 'express'
+import mongoose from 'mongoose'
 import { router } from './routes'
 import dotenv from 'dotenv'
 
@@ -9,6 +10,10 @@ app.use(router)
 
 dotenv.config()
 const port = process.env["PORT"]!
+
+mongoose.connect(process.env["DB_CONNECTION"]!).then(() => {
+  console.log("Connected to database")
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
